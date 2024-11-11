b43e258dcb15e78f161cbdccb4ab4982a8cf4261
Akuichi <verusclues@gmail.com>
2024-11-11 21:08:54 +0800

    Add files via upload

---
+++ DSA_Finals/src/main/java/group/dsa_finals/DSA_Finals.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package group.dsa_finals;
+
+/**
+ *
+ * @author Akutan
+ */
+public class DSA_Finals {
+
+    public static void main(String[] args) {
+        var visualNovel = new SceneManager(0);
+        
+    }
+}
